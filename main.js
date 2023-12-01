fe4b59c610f4fb04e804d351ad4e99a38decdc16
aqela-batata-alt <victormoreiraalmeida@gmail.com>
2023-11-30 22:11:14 -0300

    criando a branch e adicionando funcao que cria parametros

---
+++ main.js
@@ -0,0 +1,22 @@
+
+
+function mostrar_licitacoes(){
+
+}
+
+function buscar_licitacoes(){
+
+}
+
+function buscar(){
+    let busca = document.getElementById('search-input')
+    window.alert(busca)
+    window.location.search = "teste"
+    window.location.pathname = "buscar-licitacao.html"
+}
+
+
+// window.location.search     // ?seila=seila
+
+
+
