1b497fb761ff282c4870e2eb6b8a9685c9f9bbae
aqela-batata-alt <victormoreiraalmeida@gmail.com>
2023-12-06 19:46:06 -0300

    recebendo o json da internet e preparando a busca

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
+let url = "https://raw.githubusercontent.com/unb-mds/2023-2-Squad04/main/src/dados/data.json"
 
 
-function mostrar_licitacoes(){
+var licitacoes = []
 
+
+
+function barra_busca(){
+    return (window.location.searchParams('pesquisa') != null) ? `placeholder="${window.location.searchParams('pesquisa')}` : 'placeholder="Buscar Licitação"'
 }
+
+
+
+
 
 function buscar_licitacoes(){
 
+    const temp = fetch(url)
+        .then(res => res.json())
+        .then((out) => {
+            return out
+        }).catch(err => console.error(err));
+    const mostrar_licitacoes = () => {
+        temp.then((a) => {
+            licitacoes = a;
+            
+            
+            // BUSCA EM SI
+            var link = new URL(window.location)
+            var pesquisa = link.searchParams.get('pesquisa')
+            console.log("pesquisa: " + pesquisa)
+            //window.alert("aha")
+            //print("<h1>boooyaaaa!!!</h1>")
+
+
+
+          });
+        };
+    
+    mostrar_licitacoes()
 }
-
-function buscar(){
-    let busca = document.getElementById('search-input')
-    window.alert(busca)
-    window.location.search = "teste"
-    window.location.pathname = "buscar-licitacao.html"
-}
-
-
-// window.location.search     // ?seila=seila
 
 
 
