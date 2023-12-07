e5271621e967be266218bf6ef93d06158ee466f3
Carlos Eduardo <eduardokadu831@gmail.com>
2023-12-06 23:22:31 -0300

    Pequenas modificações

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,13 +19,15 @@ if (valorPesquisa) {
 
     var tagSearch = document.getElementById("pesquisar_por")
     tagSearch.value = valorPesquisa
+
+    buscar_licitacoes()
 }
 
 // function barra_busca() {
 //     return (window.location.searchParams('pesquisar_por') != null) ? `placeholder="${window.location.searchParams('pesquisar_por')}` : 'placeholder="Buscar Licitação"'
 // }
 
-buscar_licitacoes()
+
 
 
 
@@ -45,23 +47,26 @@ function buscar_licitacoes() {
             // BUSCA EM SI
             var link = new URL(window.location)
             var valorPesquisa = link.searchParams.get('pesquisar_por')
-            for(let licitacao in licitacoes){
+            for (let licitacao in licitacoes) {
                 console.log(licitacoes[licitacao]['Texto_encontrado'])
-                if(licitacoes[licitacao]['Texto_encontrado'].search(valorPesquisa) > -1){
+                if (licitacoes[licitacao]['Texto_encontrado'].search(valorPesquisa) > -1) {
                     lista_licitacoes.push(licitacoes[licitacao])
                     console.log("teste " + licitacoes[licitacao]['Texto_encontrado'])
                 }
             }
             console.log[lista_licitacoes]
             var resultados = document.getElementById("resultados")
-            for(let licitacao in lista_licitacoes){
+
+            resultados.innerHTML = `<h3 class="subtopico" style="font-size: 25px; margin:auto; text-align: center;">Aqui está o que encontrei. 👇</h3>`
+
+            for (let licitacao in lista_licitacoes) {
                 resultados.innerHTML += (`<div class="result-container d-flex justify-content-between align-items-center">
-                        <p class="mb-0">${lista_licitacoes[licitacao]['Texto_encontrado']}</p>
+                        <p class="mb-0">${lista_licitacoes[licitacao]['Texto_encontrado'].slice(0,230)}...</p>
                         <a href="#" onclick="mostrarResultados(listaResultados, i)" class="details-link" data-toggle="modal" data-target="#myModal">Mais Detalhes</a>
                     </div>
                 `);
 
-            } 
+            }
 
 
         });
