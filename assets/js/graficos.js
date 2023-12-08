d245dc28efd90b7492b7809aff2e006dbf89a373
Carlos Eduardo <eduardokadu831@gmail.com>
2023-12-07 23:25:18 -0300

    Atualização

--- assets/js/graficos.js
+++ assets/js/graficos.js
@@ -1,104 +1,120 @@
 function carregarDadosGrafico(municipioSelecionado, anoSelecionado) {
-    fetch('grafico.json')
+    fetch('https://raw.githubusercontent.com/unb-mds/2023-2-Squad04/main/src/dados/counter.json')
         .then(response => response.json())
         .then(dados => {
-            if (municipioSelecionado) {
-                dados = dados.filter(item => item.municipio === municipioSelecionado);
-            }
+            fetch('assets/json/lista_municipios.json')
+                .then(response => response.json())
+                .then(lista_municipios => {
 
-            if (anoSelecionado) {
-                dados = dados.filter(item => item.ano === anoSelecionado);
-            }
+                    console.log(municipioSelecionado)
 
-            atualizarGrafico(dados, municipioSelecionado, anoSelecionado);
+                    if (municipioSelecionado == 'Todos') {
+
+                        dados = dados.filter(item => item.municipio === municipioSelecionado && item.ano == anoSelecionado);
+
+                        atualizarGrafico(dados, municipioSelecionado, anoSelecionado);
+
+                    } else {
+
+                        var municipioEncontrado = lista_municipios.find(item => item.id === municipioSelecionado);
+
+                        console.log(municipioEncontrado);
+
+                        if (municipioEncontrado) {
+                            dados = dados.filter(item => item.municipio === municipioEncontrado.municipio && item.ano == anoSelecionado);
+                        }
+                    }
+
+                    console.log(dados);
+
+                    atualizarGrafico(dados, municipioEncontrado.municipio, anoSelecionado);
+
+                })
+                .catch(error => console.error('Erro ao carregar dados do segundo arquivo:', error));
         })
         .catch(error => console.error('Erro ao carregar dados do gráfico:', error));
 }
 
-function atualizarGrafico(dados, municipioSelecionado, anoSelecionado) {
-    fetch('assets/json/lista_municipios.json')
-        .then(response => response.json())
-        .then(municipio => {
-            if (municipioSelecionado) {
-                municipio = municipio.filter(item => item.id === municipioSelecionado);
+
+
+function atualizarGrafico(dados, municipioSelecionado, ano) {
+
+    var options = {
+        series: [{
+            name: 'Quantidade de licitações',
+            data: dados.map(item => item.qtd)
+        }],
+        chart: {
+            type: 'bar',
+            height: 350
+        },
+        plotOptions: {
+            bar: {
+                horizontal: false,
+                columnWidth: '55%',
+                endingShape: 'rounded',
+                borderRadius: 5,
+            },
+        },
+        dataLabels: {
+            enabled: false
+        },
+        stroke: {
+            show: true,
+            width: 2,
+            colors: ['transparent']
+        },
+        xaxis: {
+            categories: dados.map(item => item.mes),
+            labels: {
+                style: {
+                    colors: 'var(--txt-body)',
+                    fontSize: '14px',
+                },
+            },
+        },
+        yaxis: {
+            labels: {
+                style: {
+                    colors: 'var(--txt-body)',
+                    fontSize: '14px',
+                },
+            },
+        },
+        fill: {
+            opacity: 1,
+            colors: ['#22abfa']
+        },
+        tooltip: {
+            y: {
+                formatter: function(val) {
+                    return val
+                }
             }
+        },
+    };
 
-            var options = {
-                series: [{
-                    name: 'Quantidade de licitações',
-                    data: dados.map(item => item.valor)
-                }],
-                chart: {
-                    type: 'bar',
-                    height: 350
-                },
-                plotOptions: {
-                    bar: {
-                        horizontal: false,
-                        columnWidth: '55%',
-                        endingShape: 'rounded',
-                        borderRadius: 5,
-                    },
-                },
-                dataLabels: {
-                    enabled: false
-                },
-                stroke: {
-                    show: true,
-                    width: 2,
-                    colors: ['transparent']
-                },
-                xaxis: {
-                    categories: dados.map(item => item.mes),
-                    labels: {
-                        style: {
-                            colors: 'var(--txt-body)',
-                            fontSize: '14px',
-                        },
-                    },
-                },
-                yaxis: {
-                    labels: {
-                        style: {
-                            colors: 'var(--txt-body)',
-                            fontSize: '14px',
-                        },
-                    },
-                },
-                fill: {
-                    opacity: 1,
-                    colors: ['#22abfa']
-                },
-                tooltip: {
-                    y: {
-                        formatter: function(val) {
-                            return val
-                        }
-                    }
-                },
-            };
+    var grafico = document.querySelector("#grafico");
+    if (grafico) {
+        grafico.remove();
+    }
 
-            var grafico = document.querySelector("#grafico");
-            if (grafico) {
-                grafico.remove();
-            }
+    if (municipioSelecionado == 'Todos') {
+        var titulo_grafico = document.getElementById("titulo_grafico");
+        titulo_grafico.innerHTML = `Quantidade de licitações extraídas na Paraíba, em ${ano}.`;
 
-            if (municipioSelecionado == 'todos') {
-                var titulo_grafico = document.getElementById("titulo_grafico");
-                titulo_grafico.innerHTML = `Quantidade de licitações extraídas na Paraíba, em ${anoSelecionado}.`;
-            } else {
-                var titulo_grafico = document.getElementById("titulo_grafico");
-                titulo_grafico.innerHTML = `Quantidade de licitações extraídas em ${municipio.map(item => item.municipio)}, em ${anoSelecionado}.`;
-            }
+    } else {
+        var titulo_grafico = document.getElementById("titulo_grafico");
+        titulo_grafico.innerHTML = `Quantidade de licitações extraídas em ${municipioSelecionado}, em ${ano}.`;
+    }
 
-            var novoGrafico = document.createElement("div");
-            novoGrafico.id = "grafico";
-            document.querySelector(".graficos").appendChild(novoGrafico);
+    var novoGrafico = document.createElement("div");
+    novoGrafico.id = "grafico";
+    document.querySelector(".graficos").appendChild(novoGrafico);
 
-            var chart = new ApexCharts(novoGrafico, options);
-            chart.render();
-        })
-        .catch(error => console.error('Erro ao carregar a lista de munícipios', error));
+    var chart = new ApexCharts(novoGrafico, options);
+    chart.render();
+
 }
 
-carregarDadosGrafico("todos", 2023);
+carregarDadosGrafico("Todos", 2023);
