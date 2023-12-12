3fa831de7bcdec3ff765d8128441b3932af6db14
Carlos Eduardo <eduardokadu831@gmail.com>
2023-12-11 22:40:58 -0300

    correção parcial

--- assets/js/main.js
+++ assets/js/main.js
@@ -81,7 +81,6 @@ function buscar_licitacoes() {
     mostrar_licitacoes()
 }
 
-
 function mostrarResultados(id) {
     const temp = fetch(url)
         .then(res => res.json())
@@ -97,13 +96,6 @@ function mostrarResultados(id) {
             var modal = document.getElementById("modal-data")
             modal.innerHTML = (`
                 <p>${licitacoes[searchIndex]["Texto_encontrado"]}</p>
-            `)
-            var modal_buttons = document.getElementById("modal-buttons")
-            modal_buttons.innerHTML = (`
-
-                <button type="button" class="btn-2" style="padding: 5px 10px;"  onclick="window.location.href = 'https://www-storage.voxtecnologia.com.br/?m=sigpub.publicacao&f=334&i=${licitacoes[searchIndex]["Titulo_PDF"]}'"><i class="fa-solid fa-download"></i> Baixar PDF</button>
-                <button type="button" class="btn-3" style="padding: 5px 10px;" data-dismiss="modal">Fechar</button>
-            
             `)
 
         });
